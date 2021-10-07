ccf276d88a151c75849e3c215d54c5f3dc1a6a2d
tengzinc <tengzinc@gmail.com>
2021-10-07 10:48:26 +0800

    Lab 5 Update files

---
+++ main.py
@@ -0,0 +1,18 @@
+def say_hello():
+    print("hello")
+    
+def say_world():
+    print("world")
+    
+def say_goodbye():
+   print("goodbye")
+    
+def say_hello_world():
+    say_hello()
+    print(" ")
+    say_world()
+    
+def say_goodbye_world():
+    say_goodbye()
+    print(" ")
+    say_world()
