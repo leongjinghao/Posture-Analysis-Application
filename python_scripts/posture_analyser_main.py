55e556e9dd46fda65f94b9566e92f1fd96c4e4b5
leongjinghao <73938217+leongjinghao@users.noreply.github.com>
2022-01-13 18:10:03 +0800

    Update posture_analyser_main.py

--- python_scripts/posture_analyser_main.py
+++ python_scripts/posture_analyser_main.py
@@ -11,8 +11,7 @@ from shapely.geometry import Polygon
 from urllib3.exceptions import InsecureRequestWarning
 
 # CLI argument(s)
-# args = sys.argv[1:]
-args = [0, 1]
+args = sys.argv[1:]
 
 # video stream source
 cap = cv2.VideoCapture(args[0])
@@ -190,13 +189,13 @@ def multiPersonPostureRecognition(outputs, frame):
                 bboxColour = (255, 0, 255)
                 
                 # log bad posture detected in database through post request
-                # requests.post(
-                #     url = "https://localhost:5001/PostureLog", 
-                #     json = {'cameraId': int(args[1]),
-                #             'zone': 'danger',
-                #             'postureLandmarks': ",".join([str(lm) for lm in postureLm]),
-                #             'classification': 'bad'},
-                #     verify = False)
+                requests.post(
+                    url = "https://localhost:5001/PostureLog", 
+                    json = {'cameraId': int(args[1]),
+                            'zone': 'danger',
+                            'postureLandmarks': ",".join([str(lm) for lm in postureLm]),
+                            'classification': 'bad'},
+                    verify = False)
 
                 # if the person was performing good posture previously, 
                 # consolidate and save the good posture frames of the person into a video
@@ -225,13 +224,13 @@ def multiPersonPostureRecognition(outputs, frame):
                 bboxColour = (255, 255, 0)
                 
                 # log good posture detected in database through post request
-                # requests.post(
-                #     url = "https://localhost:5001/PostureLog", 
-                #     json = {'cameraId': int(args[1]),
-                #             'zone': 'danger',
-                #             'postureLandmarks': ",".join([str(lm) for lm in postureLm]),
-                #             'classification': 'good'},
-                #     verify = False)
+                requests.post(
+                    url = "https://localhost:5001/PostureLog", 
+                    json = {'cameraId': int(args[1]),
+                            'zone': 'danger',
+                            'postureLandmarks': ",".join([str(lm) for lm in postureLm]),
+                            'classification': 'good'},
+                    verify = False)
                 
                 # if the person was performing bad posture previously, 
                 # consolidate and save the bad posture frames of the person into a video
@@ -263,13 +262,13 @@ def multiPersonPostureRecognition(outputs, frame):
                 bboxColour = (0, 0, 255)
                 
                 # log bad posture detected in database through post request
-                # requests.post(
-                #     url = "https://localhost:5001/PostureLog", 
-                #     json = {'cameraId': int(args[1]),
-                #             'zone': 'normal',
-                #             'postureLandmarks': ",".join([str(lm) for lm in postureLm]),
-                #             'classification': 'bad'},
-                #     verify = False)
+                requests.post(
+                    url = "https://localhost:5001/PostureLog", 
+                    json = {'cameraId': int(args[1]),
+                            'zone': 'normal',
+                            'postureLandmarks': ",".join([str(lm) for lm in postureLm]),
+                            'classification': 'bad'},
+                    verify = False)
                 
                 # if the person was performing good posture previously, 
                 # consolidate and save the good posture frames of the person into a video
@@ -299,13 +298,13 @@ def multiPersonPostureRecognition(outputs, frame):
                 bboxColour = (0, 255, 0)
                 
                 # log good posture detected in database through post request
-                # requests.post(
-                #     url = "https://localhost:5001/PostureLog", 
-                #     json = {'cameraId': int(args[1]),
-                #             'zone': 'normal',
-                #             'postureLandmarks': ",".join([str(lm) for lm in postureLm]),
-                #             'classification': 'good'},
-                #     verify = False)
+                requests.post(
+                    url = "https://localhost:5001/PostureLog", 
+                    json = {'cameraId': int(args[1]),
+                            'zone': 'normal',
+                            'postureLandmarks': ",".join([str(lm) for lm in postureLm]),
+                            'classification': 'good'},
+                    verify = False)
 
                 # if the person was performing bad posture previously, 
                 # consolidate and save the bad posture frames of the person into a video
