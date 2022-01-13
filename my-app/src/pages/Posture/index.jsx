14fdfbce428a81f04322a7f9986dc524da629a7f
Zi Ying <tengziying97@gmail.com>
2022-01-13 16:18:57 +0800

    Configured to show videos.

--- my-app/src/pages/Posture/index.jsx
+++ my-app/src/pages/Posture/index.jsx
@@ -5,18 +5,9 @@ export default () => {
   return (
     <>
       <ProCard style={{ marginTop: 8 }} gutter={[16, 16]} wrap title="List of Videos" >
-        <ProCard colSpan={{ xs: 24, sm: 12, md: 12, lg: 12, xl: 12 }} layout="center" bordered>
-          Col
-        </ProCard>
-        <ProCard colSpan={{ xs: 24, sm: 12, md: 12, lg: 12, xl: 12 }} layout="center" bordered>
-          Col
-        </ProCard>
-        <ProCard colSpan={{ xs: 24, sm: 12, md: 12, lg: 12, xl: 12 }} layout="center" bordered>
-          Col
-        </ProCard>
-        <ProCard colSpan={{ xs: 24, sm: 12, md: 12, lg: 12, xl: 12 }} layout="center" bordered>
-          Col
-        </ProCard>
+        <video width="auto" height="auto" controls>
+          <source src='/video_sample/boxing.mp4' type='video/mp4'/>
+        </video>
       </ProCard>
     </>
   );
