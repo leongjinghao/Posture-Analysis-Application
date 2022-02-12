3944d8c39b3524ac065d7240aa15dbab4d94ab6a
leongjinghao <73938217+leongjinghao@users.noreply.github.com>
2022-02-12 20:35:07 +0800

    Added sample line for execution of posture recognition python script with flask endpoint url

--- start_instances.sh
+++ start_instances.sh
@@ -2,3 +2,4 @@
 
 python python_scripts/posture_analyser_main.py "python_scripts/video_sample/dancing2.mp4" 2 &
 python python_scripts/posture_analyser_main.py "python_scripts/video_sample/cycling.mp4" 1 &
+# python python_scripts/posture_analyser_main.py "http://localhost:5002/video" 2 &
