1f1e77555109a78cc62b4878e41a823301893d86
Zi Ying <tengziying97@gmail.com>
2022-02-16 17:19:52 +0800

    Include dropdown menu (not working)

--- my-app/src/pages/Posture/index.jsx
+++ my-app/src/pages/Posture/index.jsx
@@ -1,6 +1,10 @@
 import React, { Component, useState } from "react";
 import ProCard from "@ant-design/pro-card";
 import Video from './video';
+import menuClass from "./dropdown";
+import { Dropdown, Button } from 'antd';
+import { DownOutlined } from '@ant-design/icons';
+import './index.css';
 
 class App extends Component {
     constructor() {
@@ -9,16 +13,20 @@ class App extends Component {
 
     render() {
         return (
+            // Fix the logic
             <ProCard tabs={{ type: 'card' }}>
                 <ProCard.TabPane key=" tab1" tab="Live Feed">
-                    <div>
-                        <img src="http://localhost:5003/video" width="50%" />
-                    </div>
-                    <div>
-                        <img src="http://localhost:5003/video" width="50%" />
-                    </div>
-                    <div>
-                        <img src="http://localhost:5003/video" width="50%" />
+                    <div class='text-dropdown'>
+                        <div class='text'>
+                            <p class='Title'>Camera 1</p>
+                        </div>
+                        <div class='dropdown1'>
+                            <Dropdown overlay={menuClass}>
+                                <Button>
+                                    All Cameras <DownOutlined />
+                                </Button>
+                            </Dropdown>
+                        </div>
                     </div>
                     <div>
                         <img src="http://localhost:5003/video" width="50%" />
