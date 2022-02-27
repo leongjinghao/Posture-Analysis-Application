817fae8f19491853a2d6f24e375828c34ec1a198
Joey Tan <joeytan55555@gmail.com>
2022-02-27 22:09:33 +0800

    Update Posture
    not working

--- my-app/src/pages/Posture/Select.js
+++ my-app/src/pages/Posture/Select.js
@@ -1,4 +1,5 @@
 import { Select } from 'antd';
+import React, { Component, useState, props } from "react";
 
 const { Option } = Select;
 
@@ -6,16 +7,17 @@ function handleChange(value) {
     console.log(`selected ${value}`);
 }
 
-function selectCamera() {
-    return (
-        <>
-            <Select defaultValue="All Cameras" style={{ width: 120 }} onChange={handleChange}>
-                <Option value="Camera 1">Camera 1</Option>
-                <Option value="Camera 2">Camera 2</Option>
-                <Option value="Camera 3">Camera 3</Option>
-            </Select>
-        </>
-    )
+class selectCamera extends React.Component  {
+    render() {
+        return (
+            <>
+                <Select defaultValue="All Cameras" style={{ width: 120 }} onChange={handleChange}>
+                    <Option value="Camera 1">Camera 1</Option>
+                    <Option value="Camera 2">Camera 2</Option>
+                </Select>
+            </>
+        )
+    }
 }
 
 export default selectCamera
