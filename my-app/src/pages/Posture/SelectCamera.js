bd6bed26acf8a2998274ef58e9f823ffb4983af9
Zi Ying <tengziying97@gmail.com>
2022-02-27 22:37:44 +0800

    Edited again with comments

--- my-app/src/pages/Posture/SelectCamera.js
+++ my-app/src/pages/Posture/SelectCamera.js
@@ -3,6 +3,11 @@ import React, { Component, useState, props } from "react";
 
 const { Option } = Select;
 
+/**
+ * Add new camera link to this array. Haven't tested!
+ */
+const cameraLink = { "Camera 1": "http://localhost:5003/video", "Camera 2": "http://localhost:5003/video" }
+
 // function handleChange(value) {
 //     console.log(`selected ${value}`);
 // }
@@ -16,8 +21,12 @@ class SelectCamera extends React.Component {
         camera: '',
     };
 
+    /**
+     * Haven't really tested it out!
+     */
     handleChange(value) {
-        console.log(`selected ${value}`);
+        // console.log(`selected ${value}`);
+        camera = cameraLink[value];
     }
 
     render() {
@@ -29,14 +38,15 @@ class SelectCamera extends React.Component {
                             <p class='Title'>Camera 1</p>
                         </div>
                         <div class='dropdown1'>
-                            <Select defaultValue="All Cameras" style={{ width: 120 }} onChange={handleChange}>
+                            <Select defaultValue="All Cameras" style={{ width: 120 }} onChange={this.handleChange}>
                                 <Option value="Camera 1">Camera 1</Option>
                                 <Option value="Camera 2">Camera 2</Option>
+                                {/* Add <Option /> to add new camera */}
                             </Select>
                         </div>
                     </div>
                     <div>
-                        <img src="http://localhost:5003/video" />
+                        <img src={this.state.url} />
                     </div>
                 </div>
             </>
