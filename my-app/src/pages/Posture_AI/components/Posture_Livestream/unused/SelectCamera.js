aef32e56dc7d8cac04efdb82c7b130c1ecf835d1
Zi Ying <tengziying97@gmail.com>
2022-03-08 17:40:10 +0800

    store this to unused folder

---
+++ my-app/src/pages/Posture_AI/components/Posture_Livestream/unused/SelectCamera.js
@@ -0,0 +1,56 @@
+import { Select } from 'antd';
+import React, { Component, useState, props } from "react";
+
+const { Option } = Select;
+
+/**
+ * Add new camera Url to this array
+ */
+const cameraUrl = { 'Camera 1': "http://localhost:5003/video", 'Camera 2': "add new link" }
+
+class SelectCamera extends React.Component {
+    formRef = React.createRef();
+    /**
+     * default url and camera
+     */
+    state = {
+        url: 'http://localhost:5003/video',
+        camera: 'Camera 1',
+    };
+
+    /**
+     * Haven't really tested it out!
+     */
+    handleChange = value => {
+        this.setState({
+            camera: value,
+            url: cameraUrl[value],
+        })
+    }
+
+    render() {
+        const { camera, url } = this.state;
+        return (
+            <>
+                <div class='container'>
+                    <h1 id="livestream_title"> <u>{this.state.title}</u> </h1>
+                    <div class='text'>
+                        <p class='Title'>{camera}</p>
+                    </div>
+                    <div class='dropdown1'>
+                        <Select defaultValue="Camera 1" style={{ width: 120 }} onChange={this.handleChange}>
+                            <Option value="Camera 1">Camera 1</Option>
+                            <Option value="Camera 2">Camera 2</Option>
+                            {/* Add <Option /> to add new camera */}
+                        </Select>
+                    </div>
+                </div>
+                <div>
+                    <img src={url} />
+                </div>
+            </>
+        )
+    }
+}
+
+export default SelectCamera
